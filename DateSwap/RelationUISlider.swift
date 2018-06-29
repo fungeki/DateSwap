147a7dc7f0b95af4e1d5e481c6673da49a626e78
fungeki <fang.society@gmail.com>
2018-06-29 20:12:03 +0300

    slider

---
+++ DateSwap/RelationUISlider.swift
@@ -0,0 +1,29 @@
+//
+//  RelationUISlider.swift
+//  DateSwap
+//
+//  Created by Ran Loock on 29/06/2018.
+//  Copyright © 2018 Trisk Quality. All rights reserved.
+//
+
+import UIKit
+
+class RelationUISlider: UISlider {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+    @IBInspectable
+    var trackHeight: CGFloat = 8
+    
+    override func trackRect(forBounds bounds: CGRect) -> CGRect {
+        var newBounds = super.trackRect(forBounds: bounds)
+        newBounds.size.height = trackHeight
+        return newBounds
+    }
+    
+}

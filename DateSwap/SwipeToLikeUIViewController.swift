e0be3fbe773d0ea8ed69a1a0200c510cf2412ded
fungeki <fang.society@gmail.com>
2018-06-29 14:16:40 +0300

    dasd

--- DateSwap/SwipeToLikeUIViewController.swift
+++ DateSwap/SwipeToLikeUIViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 
 class SwipeToLikeUIViewController: UIViewController {
+    @IBOutlet weak var factionIndicatorUIImage: UIImageView!
     @IBOutlet weak var productCardUIView: ProductCardUIView!
+
+ 
     
     @IBOutlet weak var mainImageUIImageView: UIImageView!
     override func viewDidLoad() {
@@ -25,7 +28,78 @@ class SwipeToLikeUIViewController: UIViewController {
     override func viewDidLayoutSubviews() {
         mainImageUIImageView.layer.cornerRadius = productCardUIView.cornerRadius
     }
+    @IBAction func panCard(_ sender: UIPanGestureRecognizer) {
+        let card = sender.view!
+        let point = sender.translation(in: view)
+        let xFromCenter = card.center.x - view.center.x
+        let percOff = abs(xFromCenter) / view.center.x
+        
+        if xFromCenter < 0.0{
+            factionIndicatorUIImage.image = #imageLiteral(resourceName: "left")
+            factionIndicatorUIImage.tintColor = UIColor.red
+        }
+        else{
+            factionIndicatorUIImage.image = #imageLiteral(resourceName: "right")
+            factionIndicatorUIImage.tintColor = UIColor.green
+        }
+        card.center = CGPoint(x: view.center.x + point.x, y: view.center.y + point.y)
+        card.transform = CGAffineTransform(rotationAngle: 0.7 * xFromCenter / view.center.x)
+        factionIndicatorUIImage.alpha = percOff
+        if percOff > 0.7{
+            sender.isEnabled = false
+//            UIView.animate(withDuration: 0.3, animations: {
+//                self.productCardUIView.alpha = 0
+//                if xFromCenter > 0 {
+//                    UIView.animate(withDuration: 0.2, animations: {
+//                        card.center = CGPoint(x: card.center.x + 100, y: card.center.y)
+//                    })
+//                } else {
+//                    UIView.animate(withDuration: 0.2, animations: {
+//                        card.center = CGPoint(x: card.center.x - 100, y: card.center.y)
+//                    })
+//                }
+//            }, completion: {(true) in
+//
+//            })
 
+            UIView.animate(withDuration: 0.3, delay: 0, options: .allowAnimatedContent, animations: {
+                card.alpha = -2
+                if xFromCenter > 0 {
+                    card.center = CGPoint(x: card.center.x + 150, y: card.center.y)
+                } else {
+                    card.center = CGPoint(x: card.center.x - 150, y: card.center.y)
+                }
+            }, completion: { (true) in
+                card.transform = CGAffineTransform(rotationAngle: 0)
+                self.factionIndicatorUIImage.alpha = 0
+                card.center.x = self.view.center.x
+                card.center.y = self.view.center.y
+                sender.isEnabled = false
+                UIView.animate(withDuration: 0.2, animations: {
+                    card.alpha = 1
+                    sender.isEnabled = true
+                })
+            })
+
+            
+        }
+        
+        
+        //let go
+        if sender.state == UIGestureRecognizerState.ended{
+            UIView.animate(withDuration: 0.1, animations: {
+                card.transform = CGAffineTransform(rotationAngle: 0)
+            })
+            UIView.animate(withDuration: 0.3, animations: {
+                self.factionIndicatorUIImage.alpha = 0
+                
+            })
+        UIView.animate(withDuration: 0.3, delay: 0, usingSpringWithDamping: 0.7, initialSpringVelocity: 4, options: .curveLinear, animations: {
+            card.center = self.view.center
+            
+        })
+    }
+    }
     /*
     // MARK: - Navigation
 

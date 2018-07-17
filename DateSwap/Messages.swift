f67e4712371a18aa03b75154279d606297555a7c
fungeki <fang.society@gmail.com>
2018-07-17 17:04:04 +0300

    with objects

---
+++ DateSwap/Messages.swift
@@ -0,0 +1,18 @@
+//
+//  Messages.swift
+//  DateSwap
+//
+//  Created by Ran Loock on 17/07/2018.
+//  Copyright © 2018 Trisk Quality. All rights reserved.
+//
+
+import Foundation
+
+struct Message{
+    var msgID: String
+    var senderID: String
+    var receiverID: String
+    var text: String
+    var time: Int
+    var productID: String
+}
---
+++ DateSwap/Rating.swift
@@ -0,0 +1,16 @@
+//
+//  Rating.swift
+//  DateSwap
+//
+//  Created by Ran Loock on 17/07/2018.
+//  Copyright © 2018 Trisk Quality. All rights reserved.
+//
+
+import Foundation
+
+struct Review{
+    var ID: String
+    var stars: Int
+    var text: String
+    var productID: String
+}

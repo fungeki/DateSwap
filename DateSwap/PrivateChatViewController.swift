022ba087c1fab334d3a526c0609bd4baa5af2669
dor tzemach <tzemachdor@gmail.com>
2018-09-01 00:16:14 +0300

    sfsfsfsfsdff

--- DateSwap/PrivateChatViewController.swift
+++ DateSwap/PrivateChatViewController.swift
@@ -42,12 +42,13 @@ extension PrivateChatViewController : UITableViewDelegate, UITableViewDataSource
         cell.messagePrivateChatEditableLableUILable.layer.masksToBounds = true
         cell.profileLeftSenderConditionUIButton.layer.masksToBounds = true
         cell.messagePrivateChatEditableLableUILable.layer.cornerRadius = 20
-        cell.profileLeftSenderConditionUIButton.layer.cornerRadius = cell.profileLeftSenderConditionUIButton.frame.height/1.72
+        cell.profileLeftSenderConditionUIButton.layer.cornerRadius = cell.profileLeftSenderConditionUIButton.frame.height/2
         cell.selectionStyle = .none
         
         if (modelMsg.senderID == "5"){
             cell.messagePrivateChatEditableLableUILable.layer.backgroundColor = lightOrangeOpacityManual(alphaColor: 0.40)
             cell.profileLeftSenderConditionUIButton.isHidden = true
+         
             
         }else{
             cell.messagePrivateChatEditableLableUILable.layer.backgroundColor = grayTwo()

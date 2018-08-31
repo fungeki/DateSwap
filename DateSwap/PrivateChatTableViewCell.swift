4bd523cac6c40d7dec05808f9f1650c83f725571
fungeki <fang.society@gmail.com>
2018-08-31 19:38:44 +0300

    meows

---
+++ DateSwap/PrivateChatTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  PrivateChatTableViewCell.swift
+//  DateSwap
+//
+//  Created by Dor tzemach on 31/08/2018.
+//  Copyright © 2018 Trisk Quality. All rights reserved.
+//
+
+import UIKit
+
+class PrivateChatTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileLeftSenderConditionUIButton: ConditionUIButton!
+    @IBOutlet weak var messagePrivateChatEditableLableUILable: EditableLableUILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}

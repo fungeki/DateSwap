342dbcd55f966e6f47a4cfb7aed2cbb641aaf12d
dor tzemach <tzemachdor@gmail.com>
2018-08-31 14:40:13 +0300

    sfsdfs

---
+++ DateSwap/PrivateChatTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PrivateChatTableViewCell.swift
+//  DateSwap
+//
+//  Created by Dor tzemach on 31/08/2018.
+//  Copyright © 2018 Trisk Quality. All rights reserved.
+//
+
+import UIKit
+
+class PrivateChatTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
